be88bdbc1acdf7c3439d64a522d243f0530e0e8b
PradnyaTawar <tawar.rani96@gmail.com>
2021-07-07 12:07:19 +0530

    view one customer details

---
+++ viewone.php
@@ -0,0 +1,161 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['submit']))
+{
+    $from = $_GET['id'];
+    $to = $_POST['to'];
+    $amount = $_POST['amount'];
+
+    $sql = "SELECT * from users where id=$from";
+    $query = mysqli_query($conn,$sql);
+    $sql1 = mysqli_fetch_array($query); // returns array or output of user from which the amount is to be transferred.
+
+    $sql = "SELECT * from users where id=$to";
+    $query = mysqli_query($conn,$sql);
+    $sql2 = mysqli_fetch_array($query);
+
+
+
+    // constraint to check input of negative value by user
+    if (($amount)<0)
+   {
+        echo '<script type="text/javascript">';
+        echo ' alert("Oops! Negative values cannot be transferred")';  // showing an alert box.
+        echo '</script>';
+    }
+
+
+  
+    // constraint to check insufficient balance.
+    else if($amount > $sql1['balance']) 
+    {
+        
+        echo '<script type="text/javascript">';
+        echo ' alert("Bad Luck! Insufficient Balance")';  // showing an alert box.
+        echo '</script>';
+    }
+    
+
+
+    // constraint to check zero values
+    else if($amount == 0){
+
+         echo "<script type='text/javascript'>";
+         echo "alert('Oops! Zero value cannot be transferred')";
+         echo "</script>";
+     }
+
+
+    else {
+        
+                // deducting amount from sender's account
+                $newbalance = $sql1['balance'] - $amount;
+                $sql = "UPDATE users set balance=$newbalance where id=$from";
+                mysqli_query($conn,$sql);
+             
+
+                // adding amount to reciever's account
+                $newbalance = $sql2['balance'] + $amount;
+                $sql = "UPDATE users set balance=$newbalance where id=$to";
+                mysqli_query($conn,$sql);
+                
+                $sender = $sql1['name'];
+                $receiver = $sql2['name'];
+                $sql = "INSERT INTO transaction(`sender`, `receiver`, `balance`) VALUES ('$sender','$receiver','$amount')";
+                $query=mysqli_query($conn,$sql);
+
+                if($query){
+                     echo "<script> alert('Transaction Successful');
+                                     window.location='transactionhistory.php';
+                           </script>";
+                    
+                }
+
+                $newbalance= 0;
+                $amount =0;
+        }
+    
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <link rel="stylesheet"href="bootstrap/css/bootstrap.min.css"type="text/css">
+      <script type="text/javascript" src="bootstrap/jquery-3.5.1.min.js"></script>
+      <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+      <link href="css/index.css"rel="stylesheet"type="text/css">
+    <title>Banking System</title>
+  </head>
+  
+    <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a href="index.php" class="navbar-brand" href="#">Basic Banking System</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                        <li><a href="viewallcustomers.php"><span class="glyphicon glyphicon-user"></span> User</a></li>
+                        <li><a href="transfermoney.php"><span class="glyphicon glyphicon-log-in"></span>Transfer Money</a></li>
+
+                    </ul>
+        </div>
+      </div>
+    </nav>
+
+<body style="background-color :#e3c946  ;">
+
+	<div class="containerfor">
+    <div class="eleven">
+  <h2>Your Account</h2>
+</div>  
+            <?php
+                include 'config.php';
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM  users where id=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error : ".$sql."<br>".mysqli_error($conn);
+                }
+                $rows=mysqli_fetch_assoc($result);
+            ?>
+            <form method="post" name="tcredit" class="tabletext" ><br>
+        <div>
+            <table class="table table-striped table-condensed table-bordered">
+                <tr style="color : black;">
+                    <th class="text-center">Id</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Balance</th>
+                </tr>
+                <tr style="color : black;">
+                    <td class="py-2"><?php echo $rows['id'] ?></td>
+                    <td class="py-2"><?php echo $rows['name'] ?></td>
+                    <td class="py-2"><?php echo $rows['email'] ?></td>
+                    <td class="py-2"><?php echo $rows['balance'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <div class="container-fluid" id="content">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <h3 align="center">Your Account details are here. Have a Great Day.</h3><hr>
+                 
+                    
+                </div>
+            </div>
+        </div>
+      
+        
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
