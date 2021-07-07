025f4ef16c49f8b92f35df903464a9300212bd01
PradnyaTawar <tawar.rani96@gmail.com>
2021-07-07 11:48:35 +0530

    home page created

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <link rel="stylesheet"href="bootstrap/css/bootstrap.min.css"type="text/css">
+      <script type="text/javascript" src="bootstrap/jquery-3.5.1.min.js"></script>
+      <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+      <link href="css/index.css"rel="stylesheet"type="text/css">
+    <title>Banking System</title>
+  </head>
+    <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a href="index.html" class="navbar-brand" href="#">Basic Banking System</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                        <li><a href="viewallcustomers.php"><span class="glyphicon glyphicon-user"></span> User</a></li>
+                        <li><a href="transfermoney.php"><span class="glyphicon glyphicon-log-in"></span>Transfer Money</a></li>
+
+                    </ul>
+        </div>
+      </div>
+    </nav>
+    
+     <div class="content">
+    <div class="banner-image">
+        <div class="inneer-banner-image">
+            <center>
+            <div class="banner-content">
+                <h1>Basic Banking System</h1>
+                <p>Task:1 The Sparks Foundation</p>
+                <a href="index.php" >
+                <button type="button" class="btn btn-info btn-rounded btn-lg">Get Started<i class="fas fa-heart ml-2" aria-hidden="true"></i></button>                
+            </div>
+            </center>
+        </div>
+        </div>
+    </div>
+    
+    <blockquote class="blockquote text-center">
+        <h2> Satisfy your customers' expectations with personalized financial services </h2>
+        <footer class="blockquote-footer">Someone famous in <cite title="Source Title">tk</cite></footer>
+      </blockquote>
+    <div class="containertoo">
+        <div class="items">
+            <a href="viewallcustomers.php" >
+                <img src="customers.png" alt="" class="thumbnail">
+                <div class="caption">
+                <button type="button" class="btn btn-info btn-rounded btn-lg">All Customers<i class="fas fa-heart ml-2" aria-hidden="true"></i></button>                
+                </div>
+            </a>
+        </div>
+        <div class="items">
+            <a href="transfermoney.php" >
+                <img src="transfer.jpg" alt="" class="thumbnail">
+                <div class="caption">
+                <button type="button" class="btn btn-info btn-rounded btn-lg">Money Transfer<i class="fas fa-heart ml-2" aria-hidden="true"></i></button>                
+                </div>
+            </a>
+        </div>
+        <div class="items">
+            <a href="transactionhistory.php" >
+                <img src="history.png" alt="" class="thumbnail">
+                <div class="caption">
+                <button type="button" class="btn btn-info btn-rounded btn-lg">Transaction History<i class="fas fa-heart ml-2" aria-hidden="true"></i></button>                
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+    <footer class="footer">
+        <div class="container">
+           <center>
+            <p>“Copyright ©PradnyaTawar. All Rights Reserved | Contact Us: +91 7350284384”.</p>
+            </center>
+        </div>
+    </footer>
+    </body>
+</html>
+    
+    
